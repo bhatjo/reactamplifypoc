3295173ecf8af01064fcacc7a0c4ec4920a92145
Jyotsna Bhat <jyotsna.bhat@riotinto.com>
2020-11-30 15:45:22 +1000

    testing changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Testing redeploy!</h1>
+        <h1>Testing redeploy now!</h1>
       </header>
       <AmplifySignOut />
     </div>
